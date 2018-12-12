b64243a9497438a1e06c8e3c8272a001298abc75
wittaya1999 <45808914+wittaya1999@users.noreply.github.com>
2018-12-12 14:45:30 +0700

    Add files via upload

--- test_ss.php
+++ test_ss.php
@@ -7,7 +7,7 @@ $server = "us-cdbr-iron-east-01.cleardb.net";
  $conn = new mysqli($server, $username, $password, $db);
  mysqli_query($conn, "SET NAMES utf8");
  
- $sql_text = "SELECT * FROM contacttable WHERE name = 'วิท' ";
+ $sql_text = "SELECT * FROM contacttable WHERE name LIKE '%วิ%' ";
  $query_text = mysqli_query ($conn,$sql_text);
  
  while($obj = mysqli_fetch_array($query_text))
