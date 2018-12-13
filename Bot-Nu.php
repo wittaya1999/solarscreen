afe40189cce4e0f8f35c807c1a41958463a24215
wittaya1999 <45808914+wittaya1999@users.noreply.github.com>
2018-12-13 11:21:58 +0700

    Add files via upload

--- Bot-Nu.php
+++ Bot-Nu.php
@@ -1,24 +1,24 @@
 <?php
 	function send_back($txtin,$replyToken)
 	{
-	$access_token = 'd7CCCoAUOR6vaIw/BU7v0YcB4VupSJawiR5C8NT21t+1cxcPz/hwSOtca42GZMQLPonJmgeixJX67XqM9XWTbWtMxPyPcdfuQVP8E1JHKndd0Q2X26k92/uCEYubht3wBpLo9tlY2o0/MT87OV1UggdB04t89/1O/w1cDnyilFU=';
-    $messages = ['type' => 'text','text' => $txtin];//สร้างตัวแปร 
-    $url = 'https://api.line.me/v2/bot/message/reply';
-    $data = [
-                'replyToken' => $replyToken,
-                'messages' => [$messages],
-            ];
-    $post = json_encode($data);
-    $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-    $result = curl_exec($ch);
-    curl_close($ch);
-    echo $result . "\r\n";
+		$access_token = 'd7CCCoAUOR6vaIw/BU7v0YcB4VupSJawiR5C8NT21t+1cxcPz/hwSOtca42GZMQLPonJmgeixJX67XqM9XWTbWtMxPyPcdfuQVP8E1JHKndd0Q2X26k92/uCEYubht3wBpLo9tlY2o0/MT87OV1UggdB04t89/1O/w1cDnyilFU=';
+		$messages = ['type' => 'text','text' => $txtin];//สร้างตัวแปร 
+		$url = 'https://api.line.me/v2/bot/message/reply';
+		$data = [
+					'replyToken' => $replyToken,
+					'messages' => [$messages],
+				];
+		$post = json_encode($data);
+		$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+		$result = curl_exec($ch);
+		curl_close($ch);
+		echo $result . "\r\n";
 	}
      $content = file_get_contents('php://input');//รับข้อมูลจากไลน์
      $events = json_decode($content, true);//แปลง json เป็น php
@@ -33,4 +33,5 @@
 				 send_back($txtin,$replyToken);
 			 }
 		 }
+	 }
 ?>	 
