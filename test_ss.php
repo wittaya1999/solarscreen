aa13c893a448825fa4bd0cfaf625eaa3eceac60c
wittaya1999 <45808914+wittaya1999@users.noreply.github.com>
2018-12-13 14:24:28 +0700

    Add files via upload

--- test_ss.php
+++ test_ss.php
@@ -1,19 +1,18 @@
 <?php
-
-$server = "us-cdbr-iron-east-01.cleardb.net";
- $username = "b798786b8aa714";
- $password = "2e0e0451";
- $db = "heroku_ce52199dd4f50e1";
- $conn = new mysqli($server, $username, $password, $db);
- mysqli_query($conn, "SET NAMES utf8");
  function search($txtin)
  {
-		$sql_text = "SELECT * FROM contacetable WHERE name LIKE '%".$txtin."%'";
-		$query = mysqli_query($conn,$sql_text);
-  while ($obj_result = mysqli_fetch_array($query))
-  {
-   $result = $result."\n".$obj_result["contac_no"];
-  }
-  return $result;
+	$server = "us-cdbr-iron-east-01.cleardb.net";
+	$username = "b798786b8aa714";
+	$password = "2e0e0451";
+	$db = "heroku_ce52199dd4f50e1";
+	$conn = new mysqli($server, $username, $password, $db);
+	mysqli_query($conn, "SET NAMES utf8");
+	$sql_text = "SELECT * FROM contacetable WHERE name LIKE '%".$txtin."%'";
+	$query = mysqli_query($conn,$sql_text);
+	while ($obj_result = mysqli_fetch_array($query))
+	{
+		$result = $result."\n".$obj_result["contac_no"];
+	}
+	return $result;
  }
  ?>
